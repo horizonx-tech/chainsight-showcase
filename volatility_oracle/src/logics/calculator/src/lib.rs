6087dfd4ee5fbcabd3e2e8212c202dc8d2fa5dec
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-09-29 18:41:58 +0900

    store secrets

--- volatility_oracle/src/logics/calculator/src/lib.rs
+++ volatility_oracle/src/logics/calculator/src/lib.rs
@@ -1,10 +1,15 @@
 #[derive(Clone, Debug, Default, candid :: CandidType, serde :: Deserialize, serde :: Serialize)]
-pub struct Account {
-    pub address: String,
+pub struct Price {
+    pub observation_index: u16,
+    pub sqrt_price_x96: u128,
+    pub block_timestamp: u64,
 }
-use uniswap_snapshotter_mainnet_bindings::get_uniswap_snapshotter_mainnet;
-pub async fn calculate(targets: Vec<String>) -> Account {
-    let _result: Result<String, String> =
-        get_uniswap_snapshotter_mainnet(targets.get(0).unwrap().clone()).await;
-    todo!()
+use uniswap_snapshotter_mainnet_bindings::{
+    get_uniswap_snapshotter_mainnet, get_uniswap_snapshotter_mainnet_unwrap,
+};
+pub async fn calculate(targets: Vec<String>) -> Price {
+    let mainnet = get_uniswap_snapshotter_mainnet(targets.get(0).unwrap().clone())
+        .await
+        .unwrap();
+    todo!();
 }
