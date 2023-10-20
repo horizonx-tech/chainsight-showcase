dfd001148385dbaad4cead2cf4c6e19d762ace9c
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-10-20 15:18:32 +0900

    resolve conflict

--- volatility_oracle/oracle/contracts/interfaces/IOraclePutter.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-interface IOraclePutter {
-    function updateState(uint256 price) external;
-}
--- volatility_oracle/oracle/contracts/mock/Oracle.sol
+++ volatility_oracle/oracle/contracts/mock/Oracle.sol
@@ -1,16 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
-import "../interfaces/IOraclePutter.sol";
+import "../interfaces/IOracle.sol";
 
-contract Oracle is IOraclePutter {
-    mapping(address => uint256) public values;
+contract Oracle is IOracle {
+    mapping(address => bytes) public data;
 
-    function updateState(uint256 _values) external override {
-        values[msg.sender] = _values;
+    function updateState(bytes calldata _data) external override {
+        data[msg.sender] = _data;
+        emit StateUpdated(msg.sender, _data);
     }
 
-    function get(address putter) external view returns (uint256) {
-        return values[putter];
+    function readAsString(
+        address sender
+    ) external view override returns (string memory) {
+        return abi.decode(data[sender], (string));
+    }
+
+    function readAsUint256(
+        address sender
+    ) external view override returns (uint256) {
+        return abi.decode(data[sender], (uint256));
+    }
+
+    function readAsUint128(
+        address sender
+    ) external view override returns (uint128) {
+        return abi.decode(data[sender], (uint128));
+    }
+
+    function readAsUint64(
+        address sender
+    ) external view override returns (uint64) {
+        return abi.decode(data[sender], (uint64));
     }
 }
