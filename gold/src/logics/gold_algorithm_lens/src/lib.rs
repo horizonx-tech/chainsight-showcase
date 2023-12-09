0a78f7ba64fd809d9a84c4c602d72da0ac385a5c
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-12-09 14:29:36 +0900

    add gold

---
+++ gold/src/logics/gold_algorithm_lens/src/lib.rs
@@ -0,0 +1,15 @@
+use gold_algorithm_lens_accessors::*;
+pub type LensValue = u128;
+const DECIMALS: u32 = 8;
+pub async fn calculate(targets: Vec<String>) -> LensValue {
+    let result =
+        get_get_last_snapshot_in_gold_snapshot_indexer_https(targets.get(0usize).unwrap().clone())
+            .await;
+    let market_price = result.unwrap().chart.result[0].meta.regularMarketPrice;
+    convert_to_u128(market_price)
+}
+
+fn convert_to_u128(value: f64) -> u128 {
+    let v = value * (10u128.pow(DECIMALS) as f64);
+    v.round() as u128
+}
---
+++ gold/src/logics/gold_relayer_sepolia/src/types.rs
@@ -0,0 +1,3 @@
+use gold_relayer_sepolia_bindings as bindings;
+pub type ResponseType = bindings::ResponseType;
+pub type RequestArgsType = bindings::RequestArgsType;
