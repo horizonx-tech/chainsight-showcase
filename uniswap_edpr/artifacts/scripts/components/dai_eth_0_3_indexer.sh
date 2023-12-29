98d79bc6e332181d87b926357273ad25f2d208f1
timk11 <timk@email.com>
2023-12-29 15:36:36 +1000

    update artifacts/scripts

--- uniswap_edpr/artifacts/scripts/components/dai_eth_0_3_indexer.sh
+++ uniswap_edpr/artifacts/scripts/components/dai_eth_0_3_indexer.sh
@@ -2,23 +2,23 @@
 # init
 dfx canister --network ic call dai_eth_0_3_indexer init_in '(variant { "Production" }, record {
                 refueling_interval = 86400: nat64;
-                vault_intial_supply = 450000000000: nat;
-                indexer = record { 
+                vault_intial_supply = 500000000000: nat;
+                indexer = record {
                     initial_supply = 0: nat;
-                    refueling_amount = 400000000000: nat;
-                    refueling_threshold = 200000000000: nat;
+                    refueling_amount = 500000000000: nat;
+                    refueling_threshold = 250000000000: nat;
                 };
-                db = record { 
-                    initial_supply = 600000000000: nat;
-                    refueling_amount = 400000000000: nat;
-                    refueling_threshold = 200000000000: nat;
+                db = record {
+                    initial_supply = 750000000000: nat;
+                    refueling_amount = 500000000000: nat;
+                    refueling_threshold = 250000000000: nat;
                 };
-                proxy = record { 
-                    initial_supply = 120000000000: nat;
-                    refueling_amount = 40000000000: nat;
-                    refueling_threshold = 40000000000: nat;
+                proxy = record {
+                    initial_supply = 150000000000: nat;
+                    refueling_amount = 50000000000: nat;
+                    refueling_threshold = 50000000000: nat;
                 };
-        })' --with-cycles 1200000000000 --wallet $(dfx identity get-wallet --network ic)
+        })' --with-cycles 1400000000000 --wallet $(dfx identity get-wallet --network ic)
 # setup
 dfx canister --network ic call dai_eth_0_3_indexer setup "(
     \"$(dfx canister --network ic id shared_algorithm_lens)\",
