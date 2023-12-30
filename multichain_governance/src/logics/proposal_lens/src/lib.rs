970461d59856cf1828d3797b6b742ef4cd443f11
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-12-30 16:30:28 +0900

    add lens logics

--- multichain_governance/src/logics/proposal_lens/src/lib.rs
+++ multichain_governance/src/logics/proposal_lens/src/lib.rs
@@ -1,5 +1,5 @@
 use proposal_lens_accessors::*;
-use proposal_lens_bindings::proposal_factory_event_indexer_sepolia::RequestArgsType;
+use proposal_lens_bindings::proposal_factory_event_indexer_sepolia::{RequestArgsType, U256};
 
 pub type LensValue = (
     Vec<u128>,
@@ -12,13 +12,47 @@ pub type LensValue = (
 pub type CalculateArgs = (u64, u64);
 
 pub async fn calculate(targets: Vec<String>, args: CalculateArgs) -> LensValue {
-    let _result = get_events_from_to_in_proposal_factory_event_indexer_sepolia(
+    let results = get_events_from_to_in_proposal_factory_event_indexer_sepolia(
         targets.get(0usize).unwrap().clone(),
         RequestArgsType {
             0: args.0,
             1: args.1,
         },
     )
-    .await;
-    todo!()
+    .await
+    .unwrap();
+    results
+        .iter()
+        .map(|r| {
+            let block = r.0;
+            let events = &r.1;
+            let e: Vec<(u128, String, u128, u128, u128, u64)> = events
+                .iter()
+                .map(|e| {
+                    (
+                        e.id.value.parse().unwrap(),
+                        e.proposer.clone(),
+                        e.startTimestamp.value.parse().unwrap(),
+                        e.endTimestamp.value.parse().unwrap(),
+                        e.chainId.value.parse().unwrap(),
+                        block,
+                    )
+                })
+                .collect();
+            e
+        })
+        .flatten()
+        .map(|e| (e.0, e.1, e.2, e.3, e.4, e.5))
+        .fold(
+            (vec![], vec![], vec![], vec![], vec![], vec![]),
+            |mut acc, e| {
+                acc.0.push(e.0);
+                acc.1.push(e.1);
+                acc.2.push(e.2);
+                acc.3.push(e.3);
+                acc.4.push(e.4);
+                acc.5.push(e.5.into());
+                acc
+            },
+        )
 }
