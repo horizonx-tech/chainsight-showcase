e1ce2e7b5c9314416ca94b742713e923d1a3f03a
timk11 <timk@email.com>
2024-01-03 22:38:05 +1000

    update artifacts/scripts

--- uniswap_edpr/artifacts/scripts/components/dai_eth_0_3_relayer_new.sh
+++ uniswap_edpr/artifacts/scripts/components/dai_eth_0_3_relayer_new.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # init
-dfx canister --network ic call shib_eth_1_indexer init_in '(variant { "Production" }, record {
+dfx canister --network ic call dai_eth_0_3_relayer_new init_in '(variant { "Production" }, record {
                 refueling_interval = 86400: nat64;
                 vault_intial_supply = 1000000000000: nat;
                 indexer = record { 
@@ -20,9 +20,16 @@ dfx canister --network ic call shib_eth_1_indexer init_in '(variant { "Productio
                 };
         })' --with-cycles 2800000000000 --wallet $(dfx identity get-wallet --network ic)
 # setup
-dfx canister --network ic call shib_eth_1_indexer setup "(
-    \"$(dfx canister --network ic id shared_algorithm_lens)\",
-    vec { \"$(dfx canister --network ic id shib_eth_1_pool_fees)\"; \"$(dfx canister --network ic id shib_eth_1_tcumul_28x6hr)\"; \"$(dfx canister --network ic id shib_eth_1_v3pool)\"; \"$(dfx canister --network ic id eth_usdc_price)\" },
+dfx canister --network ic call dai_eth_0_3_relayer_new setup "(
+    \"0xB5Ef491939A6dBf17287666768C903F03602c550\",
+    record {
+        url = \"https://ethereum-sepolia.blockpi.network/v1/rpc/public\";
+        from = null;
+        chain_id = 11155111;
+        env = variant { Production };
+    },
+    \"$(dfx canister --network ic id dai_eth_0_3_indexer_lens)\",
+    vec { \"$(dfx canister --network ic id dai_eth_0_3_pool_fees)\"; \"$(dfx canister --network ic id dai_eth_0_3_tcumul_28x6hr)\"; \"$(dfx canister --network ic id dai_eth_0_3_v3pool)\"; \"$(dfx canister --network ic id eth_usdc_price)\" },
 )"
 # set_task
-dfx canister --network ic call shib_eth_1_indexer set_task '(7200, 5)'
+dfx canister --network ic call dai_eth_0_3_relayer_new set_task '(7200, 10)'
--- uniswap_edpr/artifacts/scripts/components/shib_eth_1_v3pool.sh
+++ uniswap_edpr/artifacts/scripts/components/shib_eth_1_v3pool.sh
@@ -2,22 +2,22 @@
 # init
 dfx canister --network ic call shib_eth_1_v3pool init_in '(variant { "Production" }, record {
                 refueling_interval = 86400: nat64;
-                vault_intial_supply = 500000000000: nat;
-                indexer = record {
+                vault_intial_supply = 1000000000000: nat;
+                indexer = record { 
                     initial_supply = 0: nat;
-                    refueling_amount = 500000000000: nat;
-                    refueling_threshold = 250000000000: nat;
+                    refueling_amount = 1000000000000: nat;
+                    refueling_threshold = 500000000000: nat;
                 };
-                db = record {
-                    initial_supply = 750000000000: nat;
-                    refueling_amount = 500000000000: nat;
-                    refueling_threshold = 250000000000: nat;
+                db = record { 
+                    initial_supply = 1500000000000: nat;
+                    refueling_amount = 1000000000000: nat;
+                    refueling_threshold = 500000000000: nat;
                 };
-                proxy = record {
-                    initial_supply = 150000000000: nat;
-                    refueling_amount = 50000000000: nat;
-                    refueling_threshold = 50000000000: nat;
+                proxy = record { 
+                    initial_supply = 300000000000: nat;
+                    refueling_amount = 100000000000: nat;
+                    refueling_threshold = 100000000000: nat;
                 };
-        })' --with-cycles 1400000000000 --wallet $(dfx identity get-wallet --network ic)
+        })' --with-cycles 2800000000000 --wallet $(dfx identity get-wallet --network ic)
 # set_task
 dfx canister --network ic call shib_eth_1_v3pool set_task '(7200, 10)'
