37d5a69500c6b329ab24d14c7e2743b6d3174128
timk11 <timk@email.com>
2024-01-04 02:22:52 +1000

    update _indexer_lens

--- uniswap_edpr/src/logics/shib_eth_1_indexer_lens/src/lib.rs
+++ uniswap_edpr/src/logics/shib_eth_1_indexer_lens/src/lib.rs
@@ -1,8 +1,7 @@
 use shib_eth_1_indexer_lens_accessors :: * ;
-# [derive (Clone , Debug , Default , candid :: CandidType , serde :: Deserialize , serde :: Serialize)]
 pub type LensValue = u64;
 pub async fn calculate (targets : Vec < String >) -> LensValue {
-  let _result = get_get_last_snapshot_in_shib_eth_1_indexer (targets . get (0usize) . unwrap () . clone ()) . await ;
+  let _result = get_get_last_snapshot_value_in_shib_eth_1_indexer (targets . get (0usize) . unwrap () . clone ()) . await ;
   let edpr = _result.unwrap().edpr;
   let edpr_int = (edpr * 10000.0) as u64;
   edpr_int
