01e1573c815a843b1593dbca09fbff6c09d02ede
megared05 <05megared@gmail.com>
2024-04-04 14:44:41 +0900

    update: rvol - bundle rvol lens

---
+++ volatility/realized_volatility/src/logics/realized_volatility_collector/src/lib.rs
@@ -0,0 +1,29 @@
+use std::collections::BTreeMap;
+
+use ic_web3_rs::futures;
+use realized_volatility_collector_accessors::*;
+use realized_volatility_collector_bindings::realized_volatility_from_u256;
+
+pub type LensValue = BTreeMap<String, Option<f64>>;
+pub type CalculateArgs = Vec<String>;
+
+const COUNT: u64 = 24; // Hourly
+
+pub async fn calculate(targets: Vec<String>, args: CalculateArgs) -> LensValue {
+    let calculator_id = targets.get(0).unwrap();
+    let results = futures::future::join_all(
+        args
+            .clone()
+            .iter()
+            .map(|target| get_rvol(calculator_id.clone(), realized_volatility_from_u256::LensArgs {
+                args: realized_volatility_from_u256::CalculateArgs { snapshot_count: COUNT },
+                targets: vec![target.clone()],
+            })),
+    ).await;
+    
+    let res_vec = args.iter().enumerate().map(|(i, target)| {
+        let result = results.get(i).unwrap();
+        (target.clone(), result.clone().ok())
+    }).collect::<Vec<(String, Option<f64>)>>();
+    res_vec.into_iter().collect()
+}
